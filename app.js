30486e97a3527a72c8b2eef69bb17243157598ee
AmmarKhalid123 <ammarkhalid237@gmail.com>
2020-06-28 01:22:58 +0500

    Passport-Local-Mongoose

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,9 +20,10 @@ const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/confusion';
-const connect = mongoose.connect(url);
+const connect = mongoose.connect(url, { useUnifiedTopology: true });
 
 connect.then((db) => {
+  
   console.log('Connected correctly to server.');
 }, (err) => {
   console.log(err);
@@ -35,7 +39,9 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 //app.use(cookieParser('12345-67890-09876-54321'));
+
 app.use(session({
 	name: 'session-id',
 	secret: '12345-67890-09876-54321',
@@ -44,31 +50,30 @@ app.use(session({
 	store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session())
+
+console.log("okayy");
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 
 function auth(req, res, next) {
   console.log(req.session);
 
-  if (!req.session.user){
+  if (!req.user){
       var err = new Error('You are not authenticated!');
-      err.status = 401;
+      err.status = 403;
       return next(err);
   }
   else{
-    if (req.session.user === 'authenticated'){
       next();
-    }
-    else{
-      var err = new Error('You are not authenticated!');  
-      err.status = 401;
-      return next(err);  
-    }
   }
 }
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use(auth);
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
