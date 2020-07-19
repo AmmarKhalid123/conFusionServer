cd55f5144ab880881c7fd971f7f3c8bedf4c2bd2
AmmarKhalid123 <ammarkhalid237@gmail.com>
2020-07-18 20:40:42 -0700

    CORS

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
+var cors = require('./cors');
 
 var router = express.Router();
 router.use(bodyParser.json());
 
 
 /* GET users listing. */
-router.route('/')
-.get(authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, function(req, res, next) {
 	User.find({})
   	.then((users) => {
 		
@@ -25,7 +25,7 @@ router.route('/')
  	})
 });
 
-router.post('/signup', function(req, res, next) {
+router.post('/signup', cors.corsWithOptions, function(req, res, next) {
 	
 	User.register(new User({username: req.body.username}), req.body.password, 
 	(err, user) => {
@@ -59,7 +59,7 @@ router.post('/signup', function(req, res, next) {
 	});
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local') ,  (req, res) => {
 	var token = authenticate.getToken({_id: req.user._id});
 
 	res.statusCode = 200;
