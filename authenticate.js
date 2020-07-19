a0c54d7552f8edaf3de1a37c6c88c08ef309a8da
AmmarKhalid123 <ammarkhalid237@gmail.com>
2020-07-19 02:10:34 -0700

    Facebook OAuth 2 Passport

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config');
 
@@ -39,8 +40,6 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
 
 exports.verifyUser = passport.authenticate('jwt', {session: false}); // this will run passport.use(new JwtStrategy...)
 
-//const adminCheck = 
-
 exports.verifyAdmin = (req, res, next) => {
     if (req.user.admin){
         return next();
@@ -51,4 +50,34 @@ exports.verifyAdmin = (req, res, next) => {
         return next(err);
     }
 };
-//adminCheck();
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+    clientID: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (!err && user != null) {
+                return done(null, user);
+            }
+            else {
+                user = new User({ username: profile.displayName });
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) => {
+                    if (err) {
+                        return done(err, false);
+                    }
+                    else {
+                        return done(null, user);
+                    }
+                })
+                return done(null, false);
+            }
+        })
+    
+    }
+));
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/confusion'
+    'mongoUrl': 'mongodb://localhost:27017/confusion',
+    'facebook': {
+        clientId: '3027922727303639',
+        clientSecret: 'f9eed4366ea1d4bc1337fc8427c77e4f'
+    }
 }
